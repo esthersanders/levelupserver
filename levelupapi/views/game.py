8f67edd2c4df10c9172e2bdc2ab193c004980f0c
estherviolin <esthernaomi.violin@gmail.com>
2020-11-24 11:36:12 -0600

    game tests working

--- levelupapi/views/game.py
+++ levelupapi/views/game.py
@@ -44,7 +44,7 @@ class Games(ViewSet):
         try:
             game.save()
             serializer = GameSerializer(game, context={'request': request})
-            return Response(serializer.data)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
 
         # If anything went wrong, catch the exception and
         # send a response with a 400 status code to tell the
@@ -70,7 +70,7 @@ class Games(ViewSet):
             serializer = GameSerializer(game, context={'request': request})
             return Response(serializer.data)
         except Exception as ex:
-            return HttpResponseServerError(ex)
+            return HttpResponseServerError(ex, status = status.HTTP_404_NOT_FOUND)
 
     def update(self, request, pk=None):
         """Handle PUT requests for a game
